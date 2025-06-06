e3f1a9139d19aabb9dce2ef5711266fec6a528ba
Barnegraver <andreasnilsen07@hotmail.com>
2025-06-06 18:18:36 +0200

    Initial commit

---
+++ build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_CyberScanner_FILE C:/Users/ander/Desktop/CyberScanner/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/CyberScanner.exe)
+set(__QT_DEPLOY_TARGET_CyberScanner_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_CyberScanner_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
